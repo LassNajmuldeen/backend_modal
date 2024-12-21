33b3a270853e8ea9c3293fdc101aabebf10ba6eb
LassNajmuldeen <lass.najm@gmail.com>
2024-12-21 01:40:07 +0100

    created download model function

--- main.py
+++ main.py
@@ -5,3 +5,17 @@ from datetime import datetime, timezone
 import requests
 import os
 
+# Download model from huggingface
+def download_model():
+    from diffusers import AutoPipelineForText2Image
+    import torch
+
+    AutoPipelineForText2Image.from_pretrained(
+    "stabilityai/sdxl-turbo", torch_dtype=torch.float16, variant="fp16")
+    
+# Create docker container that uses linux
+image = (modal.Image.debian_slim()
+        .pip_install("fastapi[standard]", "transformers", "accelerate", "diffusers", "requests")
+        .run_function(download_model))
+
+app = modal.App("sd-turbo", image=image)
